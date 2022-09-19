047c3a967f5ff3d219c4d95e4c4a2e2b0ee5138e
Elías Snorrason <eliassno@gmail.com>
2022-09-19 14:21:31 +0000

    :recycle: move common wrapper functionality to a base class
    Set default loss function at the module level.

--- cleanlab/experimental/keras.py
+++ cleanlab/experimental/keras.py
@@ -39,8 +39,41 @@ import tensorflow as tf
 import numpy as np
 from typing import Callable
 
+DEFAULT_LOSS = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-class KerasWrapperModel:
+
+class KerasWrapper:
+    """Base class for the Keras model APIs."""
+
+    def __init__(self, compile_kwargs: dict = {"loss": DEFAULT_LOSS}):
+        self.compile_kwargs = compile_kwargs
+        self.net = None
+
+    def fit(self, X, y=None, **kwargs):
+        """Note that `X` dataset object must already contain the labels as is required for standard Keras fit.
+        You can optionally provide the labels again here as argument `y` to be compatible with sklearn, but they are ignored.
+        """
+        self.net.compile(**self.compile_kwargs)
+        self.net.fit(X, **kwargs)
+
+    def predict_proba(self, X, *, apply_softmax=True, **kwargs):
+        """Set extra argument `apply_softmax` to True to indicate your network only outputs logits not probabilities."""
+        if self.net is None:
+            raise ValueError("must call fit() before predict()")
+        pred_probs = self.net.predict(X, **kwargs)
+        if apply_softmax:
+            pred_probs = tf.nn.softmax(pred_probs, axis=1)
+        return pred_probs
+
+    def predict(self, X, **kwargs):
+        pred_probs = self.predict_proba(X, **kwargs)
+        return np.argmax(pred_probs, axis=1)
+
+    def summary(self, **kwargs):
+        self.net.summary(**kwargs)
+
+
+class KerasWrapperModel(KerasWrapper):
     """Takes in a callable function to instantiate a Keras Model (using Keras functional API)
     that is compatible with :py:class:`CleanLearning <cleanlab.classification.CleanLearning>` and sklearn.
 
@@ -70,14 +103,11 @@ class KerasWrapperModel:
         self,
         model: Callable,
         model_kwargs: dict = {},
-        compile_kwargs: dict = {
-            "loss": tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        },
+        **kwargs,
     ):
         self.model = model
         self.model_kwargs = model_kwargs
-        self.compile_kwargs = compile_kwargs
-        self.net = None
+        super().__init__(**kwargs)
 
     def get_params(self, deep=True):
         return {
@@ -87,31 +117,11 @@ class KerasWrapperModel:
         }
 
     def fit(self, X, y=None, **kwargs):
-        """Note that `X` dataset object must already contain the labels as is required for standard Keras fit.
-        You can optionally provide the labels again here as argument `y` to be compatible with sklearn, but they are ignored.
-        """
         self.net = self.model(**self.model_kwargs)
-        self.net.compile(**self.compile_kwargs)
-        self.net.fit(X, **kwargs)
-
-    def predict_proba(self, X, *, apply_softmax=True, **kwargs):
-        """Set extra argument `apply_softmax` to True to indicate your network only outputs logits not probabilities."""
-        if self.net is None:
-            raise ValueError("must call fit() before predict()")
-        pred_probs = self.net.predict(X, **kwargs)
-        if apply_softmax:
-            pred_probs = tf.nn.softmax(pred_probs, axis=1)
-        return pred_probs
-
-    def predict(self, X, **kwargs):
-        pred_probs = self.predict_proba(X, **kwargs)
-        return np.argmax(pred_probs, axis=1)
-
-    def summary(self, **kwargs):
-        self.net.summary(**kwargs)
+        super().fit(X, y, **kwargs)
 
 
-class KerasWrapperSequential:
+class KerasWrapperSequential(KerasWrapper):
     """Makes any ``tf.keras.models.Sequential`` object compatible with :py:class:`CleanLearning <cleanlab.classification.CleanLearning>` and sklearn.
 
     `KerasWrapperSequential` is instantiated in the same way as a keras ``Sequential``  object, except for optional extra `compile_kwargs` argument.
@@ -126,7 +136,7 @@ class KerasWrapperSequential:
     name: str, default = None
         Name for the Keras model (same as for ``tf.keras.models.Sequential``).
 
-    compile_kwargs: dict, default = {"loss": tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)}
+    compile_kwargs: dict, default = {"loss": DEFAULT_LOSS}
         Dict of optional keyword arguments to pass into ``model.compile()`` for declaring loss, metrics, optimizer, etc.
     """
 
@@ -134,14 +144,11 @@ class KerasWrapperSequential:
         self,
         layers: list = None,
         name: str = None,
-        compile_kwargs: dict = {
-            "loss": tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-        },
+        **kwargs,
     ):
         self.layers = layers
         self.name = name
-        self.compile_kwargs = compile_kwargs
-        self.net = None
+        super().__init__(**kwargs)
 
     def get_params(self, deep=True):
         return {
@@ -155,21 +162,4 @@ class KerasWrapperSequential:
         You can optionally provide the labels again here as argument `y` to be compatible with sklearn, but they are ignored.
         """
         self.net = tf.keras.models.Sequential(self.layers, self.name)
-        self.net.compile(**self.compile_kwargs)
-        self.net.fit(X, **kwargs)
-
-    def predict_proba(self, X, *, apply_softmax=True, **kwargs):
-        """Set extra argument `apply_softmax` to True to indicate your network only outputs logits not probabilities."""
-        if self.net is None:
-            raise ValueError("must call fit() before predict()")
-        pred_probs = self.net.predict(X, **kwargs)
-        if apply_softmax:
-            pred_probs = tf.nn.softmax(pred_probs, axis=1)
-        return pred_probs
-
-    def predict(self, X, **kwargs):
-        pred_probs = self.predict_proba(X, **kwargs)
-        return np.argmax(pred_probs, axis=1)
-
-    def summary(self, **kwargs):
-        self.net.summary(**kwargs)
+        super().fit(X, y, **kwargs)
